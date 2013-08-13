a977be8b0afc819a39633f2e44f2fc85c0849677
tooninx <tooninx@gmail.com>
2013-08-13 23:22:31 +0100

    Refact static pages spec.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Demo App |" }
+
   describe "Home page" do
 
-    it "should have the h1 'Rails Demo App'" do
+    it "should have h1 content 'Rails Demo App'" do
       visit '/static_pages/home'
       expect(page).to have_selector('h1', :text => 'Rails Demo App')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Demo App | Home')
+      expect(page).to have_title("#{base_title} Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Demo App | Help')
+      expect(page).to have_title("#{base_title} Help")
     end
   end
 
@@ -37,7 +39,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Demo App | About')
+      expect(page).to have_title("#{base_title} About")
     end
   end
 
@@ -50,7 +52,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Demo App | Contact')
+      expect(page).to have_title("#{base_title} Contact")
     end
   end
 end
